5412e655d3f61d2af5f5af1395568c3e8b469801
Kent Claudio <rikysmile33@gmail.com>
2021-02-01 14:16:35 -0800

    Minor changes

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 // import Landing Page
-import LandingActivity from './screens/LandingActivity'
+import LandingActivity from "./screens/LandingActivity";
 // import Getting Started Page
-import LoginActivity from './screens/LoginActivity'
-import RegisterActivity from './screens/RegisterActivity'
+import LoginActivity from "./screens/LoginActivity";
+import RegisterActivity from "./screens/RegisterActivity";
 
-
-import 'react-native-gesture-handler';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-
+import "react-native-gesture-handler";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
 function HomeScreen() {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFDD02' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#FFDD02",
+      }}
+    >
       <LandingActivity navigation={navigation} />
       {/* <Button
         title="Go to Login"
@@ -31,37 +35,43 @@ function HomeScreen() {
 }
 
 function GettingStarted() {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#FFDD02' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#FFDD02",
+      }}
+    >
       <LoginActivity navigation={navigation} />
-
     </View>
   );
 }
 
-
 export default function App() {
   return (
-    <NavigationContainer style={{
-      backgroundColor: 'yellow'
-    }}
+    <NavigationContainer
+      style={{
+        backgroundColor: "yellow",
+      }}
     >
+      {/* Set page names here for navigation to access */}
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="GettingStarted" component={GettingStarted} />
         <Stack.Screen name="Register" component={RegisterActivity} />
       </Stack.Navigator>
-    </ NavigationContainer >
-
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFDD02',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFDD02",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
