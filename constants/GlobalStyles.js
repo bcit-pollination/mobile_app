f76a6fc34631f7397b2af49c5fbfacc9109a1484
Kent Claudio <rikysmile33@gmail.com>
2021-02-01 15:13:32 -0800

    Added GlobalStyles.js constant

---
+++ constants/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  genericPage: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  yellowBackground: {
+    backgroundColor: "#FFDD02",
+  },
+  whiteBackground: {
+    backgroundColor: "white",
+  },
+});
