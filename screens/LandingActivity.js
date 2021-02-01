59486c6f3fecfff88238a539ed2c4df2ac9143b0
Kent Claudio <rikysmile33@gmail.com>
2021-02-01 14:16:11 -0800

    Changed Image component usage to AppLogo component

--- screens/LandingActivity.js
+++ screens/LandingActivity.js
@@ -1,52 +1,55 @@
-import React from 'react'
-import { StyleSheet, View, Text, Button, Image, Linking } from 'react-native'
-import { color } from 'react-native-reanimated'
+import React from "react";
+import { StyleSheet, View, Text, Button, Image, Linking } from "react-native";
+import { color } from "react-native-reanimated";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import LoginActivity from './LoginActivity'
+import LoginActivity from "./LoginActivity";
 
+import AppLogo from '../components/AppLogo';
 
 export default function LandingActivity({ navigation }) {
+  // Leads to the management website.
+  function manageElections() {
+    console.log("manageElections()");
+    Linking.openURL("https://google.com").catch((err) =>
+      console.error("An error occurred", err)
+    );
+  }
 
+  return (
+    <View style={{ flexDirection: "column" }}>
+      <View classname="logo_large">
+          <AppLogo />
+      </View>
 
+      <View>
+        <Button
+          style={styles.signIn}
+          title="Get Started"
+          color="black"
+          onPress={() => navigation.navigate("GettingStarted")}
+        />
+      </View>
 
-    // Leads to the management website.
-    function manageElections() {
-        console.log('manageElections()')
-        Linking.openURL('https://google.com').catch((err) =>
-            console.error('An error occurred', err),
-        );
-    }
-
-
-    return (
-        <View style={{ flexDirection: 'column' }}>
-
-            <View classname='logo_large'>
-                <Image source={require('../assets/image/logo_pollination.jpg')}></Image>
-            </View>
-
-
-            <View>
-                <Button style={styles.signIn} title="Get Started"
-                    color="black" onPress={() => navigation.navigate('GettingStarted')} />
-            </View>
-
-            <View style={styles.register}>
-                <Button onPress={manageElections} title='Manage Elections' color='black' />
-            </View>
-            {/* <Button onPress={() => { console.log('onpress') }} title='Manage Elections' color='white' /> */}
-        </View >
-    )
+      <View style={styles.register}>
+        <Button
+          onPress={manageElections}
+          title="Manage Elections"
+          color="black"
+        />
+      </View>
+      {/* <Button onPress={() => { console.log('onpress') }} title='Manage Elections' color='white' /> */}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    signIn: {
-        // marginTop: 60
-    },
-    register: {
-        marginTop: 25,
-    },
+  signIn: {
+    // marginTop: 60
+  },
+  register: {
+    marginTop: 25,
+  },
 });
--- screens/LoginActivity.js
+++ screens/LoginActivity.js
@@ -1,36 +1,44 @@
-import React from 'react'
-import { StyleSheet, View, Text, Button, Image } from 'react-native'
+import React from "react";
+import { StyleSheet, View, Text, Button, Image } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import AppLogo from "../components/AppLogo";
 
 export default function LoginActivity({ navigation }) {
-    return (
-        <View style={{ flexDirection: 'column' }}>
+  return (
+    <View style={{ flexDirection: "column" }}>
+      <View classname="logo_large">
+        <AppLogo />
+      </View>
 
-            <View classname='logo_large'>
-                <Image source={require('../assets/image/logo_pollination.jpg')}></Image>
-            </View>
+      <View>
+        <Button
+          style={styles.signIn}
+          title="Sign In"
+          color="black"
+          onPress={() => navigation.navigate("Login")}
+        />
+      </View>
 
-
-            <View>
-                <Button style={styles.signIn} title="Sign In"
-                    color="black" onPress={() => navigation.navigate('Login')} />
-            </View>
-
-            <View style={styles.register}>
-                <Button title='Register' color='black' onPress={() => navigation.navigate('Register')} />
-            </View>
-            {/* <Button onPress={() => { console.log('onpress') }} title='Manage Elections' color='white' /> */}
-        </View >
-    )
+      <View style={styles.register}>
+        <Button
+          title="Register"
+          color="black"
+          onPress={() => navigation.navigate("Register")}
+        />
+      </View>
+      {/* <Button onPress={() => { console.log('onpress') }} title='Manage Elections' color='white' /> */}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    signIn: {
-        // marginTop: 60
-    },
-    register: {
-        marginTop: 25,
-    },
+  signIn: {
+    // marginTop: 60
+  },
+  register: {
+    marginTop: 25,
+  },
 });
