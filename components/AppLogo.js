885f66bb2b8271dd7367070d3134d079b3103b0b
Kent Claudio <rikysmile33@gmail.com>
2021-02-01 14:14:59 -0800

    Created AppLogo component

---
+++ components/AppLogo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Image } from 'react-native';
+
+const AppLogo = () => {
+    return (
+        <Image source={require('../assets/image/logo_pollination.jpg')} />
+    )
+}
+
+export default AppLogo;
