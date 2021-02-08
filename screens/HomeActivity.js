dedd3f59183b81061376343e7d2d159d4c766845
Kent Claudio <rikysmile33@gmail.com>
2021-02-08 11:18:10 -0800

    Updated modal in HomeActivity to now use react native paper's

--- screens/HomeActivity.js
+++ screens/HomeActivity.js
@@ -1,31 +1,56 @@
-import React, {useState} from "react";
-import { StyleSheet, View, Text, Button, Image, Linking } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Pressable } from "react-native";
+import { Button, Modal, Portal, Provider } from "react-native-paper";
 import DetailsModal from "../components/DetailsModal";
 import ElectionItem from "../components/ElectionItem";
 
 import GlobalStyles from "../constants/GlobalStyles";
 
 const HomeActivity = () => {
-   // modal is not shown at first
-   const [show, setShow] = useState(false);
+  // modal is not shown at first
+  const [show, setShow] = useState(false);
 
   const changeShowModal = () => {
     setShow(!show);
   };
 
+  const showModal = () => setShow(true);
+  const hideModal = () => setShow(false);
+
+  // TODO: add a '?' icon and place it in a corner with an onPress
+  // that details how to show election information (onLongPress)
+
   // TODO: dynamically load info to the modal from database
   return (
-    <View style={GlobalStyles.genericPage}>
-      <DetailsModal info="this is modal" visible={show} show={show} setShow={setShow} />
-      <View style={[styles.headingContainer, GlobalStyles.center]}>
-        <Text style={styles.headingText}>Active Elections</Text>
+    <Provider>
+      <View style={GlobalStyles.genericPage}>
+        {/* <DetailsModal info="this is modal" visible={show} show={show} setShow={setShow} /> */}
+        <Portal>
+          <Modal
+            visible={show}
+            onDismiss={hideModal}
+            contentContainerStyle={[GlobalStyles.center, styles.modalStyle]}
+          >
+            <Text>Thisss is modal</Text>
+            <Button
+              mode="contained"
+              onPress={hideModal}
+              style={[GlobalStyles.center, styles.button]}
+            >
+              <Text style={styles.text}>Close</Text>
+            </Button>
+          </Modal>
+        </Portal>
+        <View style={[styles.headingContainer, GlobalStyles.center]}>
+          <Text style={styles.headingText}>Active Elections</Text>
+        </View>
+        <View style={styles.electionsListContainer}>
+          <ElectionItem title="Election 1" onLongPress={showModal} />
+          <ElectionItem title="Election 2" onLongPress={showModal} />
+          <ElectionItem title="Election 3" onLongPress={showModal} />
+        </View>
       </View>
-      <View style={styles.electionsListContainer}>
-        <ElectionItem title="Election 1" onLongPress={changeShowModal} />
-        <ElectionItem title="Election 2" onLongPress={changeShowModal} />
-        <ElectionItem title="Election 3" onLongPress={changeShowModal} />
-      </View>
-    </View>
+    </Provider>
   );
 };
 
@@ -42,6 +67,19 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     width: "70%",
     maxWidth: "70%",
+  },
+  modalStyle: {
+    backgroundColor: "white",
+    margin: "auto", // centers modal
+    width: '50%',
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "black",
+    // bottom: 0,
+  },
+  text: {
+    color: "white",
   },
 });
 
