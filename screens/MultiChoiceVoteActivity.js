78e90d8d5fc1340db9db2efd8ae44a597c51aeea
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2021-02-15 01:06:44 -0800

    added checkboxes but not fully styled

--- screens/MultiChoiceVoteActivity.js
+++ screens/MultiChoiceVoteActivity.js
@@ -1,57 +1,59 @@
 import React from 'react'
-import { StyleSheet, View, Text, ToastAndroid, Platform, Alert } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import { useNavigation } from "@react-navigation/native";
-import { Snackbar } from 'react-native-paper'
-import RadioButton from '../components/RadioButton';
+import { Snackbar, Checkbox } from 'react-native-paper'
+
 import AppButton from '../components/AppButton';
 
-const items = [
-  { key: 'Pizza',
-    text: 'Pizza',
-    selected: false,
-  },
-  { key: 'Lasagna',
-    text: 'Lasagna',
-    selected: false,
-  },
-  { key: 'Gnocchi',
-    text: 'Gnocchi',
-    selected: false,
-  },
-];
-
 export default function MultiChoiceVoteActivity() {
+
   const navigation = useNavigation();
   const [visible, setVisible] = React.useState(false);
+  
+  const [choice1, setChoice1] = React.useState(false);
+  const [choice2, setChoice2] = React.useState(false);
 
-    const handleChoice = (items) => {
-      console.log("Submit Button Pressed! ");
-      console.log(items);
-      items.forEach(element => {
-        element.selected ? console.log("Submitting option: ", element.text):null;
-      });
+  const handleChoice = () => {
+    console.log("Submit Button Pressed! ");
 
-      // Call this if vote has failed
-      // onFailure();
-    }
+    // Call this if vote has failed
+    // onFailure();
+  }
 
-    const onFailure = () => {
-      setVisible(!visible);
-    }
+  const onFailure = () => {
+    setVisible(!visible);
+  }
 
-    const onDismissSnackBar = () => {
-      setVisible(false);
-    }
+  const onDismissSnackBar = () => {
+    setVisible(false);
+  }
 
     return (
       <View style={[
         styles.container, 
       ]}>
         <Text>Select multiple: </Text>
-        <RadioButton ITEMS={items} textColor='black' buttonColor='rgb(0,0,100)'/>
+
+        <View style={styles.checkboxContainer}>
+          <Checkbox.Item
+            status={choice1 ? 'checked' : 'unchecked'}
+            onPress={() => {
+              setChoice1(!choice1);
+            }}
+            label="Item 1"
+          />
+          <Checkbox.Item
+            status={choice2 ? 'checked' : 'unchecked'}
+            onPress={() => {
+              setChoice2(!choice2);
+            }}
+            label="Item 2"
+          />
+        </View>
+
         <AppButton text="Submit" onPress={
           () => {
-            handleChoice(items); 
+            handleChoice(); 
             navigation.navigate("VoteSuccess");
           }
         }/>
@@ -69,7 +71,14 @@ export default function MultiChoiceVoteActivity() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
+  item:{
+    width:"80%",
+    backgroundColor:"#fff",
+    borderRadius:20,
+    padding:10,
+    marginBottom:10,
+    flexDirection:"row",
+  },
 });
