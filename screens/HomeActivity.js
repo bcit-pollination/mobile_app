d05733ebda160ad66755858d7db40e3ceb164454
Kent Claudio <rikysmile33@gmail.com>
2021-02-15 06:08:11 -0800

    Updated HomeActivity

--- screens/HomeActivity.js
+++ screens/HomeActivity.js
@@ -6,30 +6,49 @@ import DetailsModal from "../components/DetailsModal";
 import ElectionItem from "../components/ElectionItem";
 
 import GlobalStyles from "../constants/GlobalStyles";
-import BluetoothFailModal from "../components/BluetoothFailModal";
+import BluetoothFailModal from "../components/old/BluetoothFailModal";
+import AppModal from "../components/AppModal";
 
 const HomeActivity = () => {
   const navigation = useNavigation();
   // modal is not shown at first
-  const [show, setShow] = useState(false);
+  const [showElectionDetails, setShowElectionDetails] = useState(false);
+  const [showTip, setShowTip] = useState(false);
 
-  const changeShowModal = () => {
-    setShow(!show);
-  };
+  // const changeShowDetailsModal = () => {
+  //   setShowElectionDetails(!showElectionDetails);
+  // };
 
-  const showModal = () => setShow(true);
-  const hideModal = () => setShow(false);
+  // const changeShowTipModal = () => {
+  //   setShowElectionDetails(!showElectionDetails);
+  // };
+
+  const showDetailsModal = () => setShowElectionDetails(true);
+  const hideDetailsModal = () => setShowElectionDetails(false);
+  const showTipModal = () => setShowTip(true);
+  const hideTipModal = () => setShowTip(false);
 
   // TODO: add a '?' icon and place it in a corner with an onPress
   // that details how to show election information (onLongPress)
 
+  const sampleElectionDetails =
+    "Election Details: \nJanuary 20 - March 15\nLower Mainland";
+  const sampleTip =
+    "Long press one of the election items to show further details about an election.";
+
   // TODO: dynamically load info to the modal from database
   return (
     <Provider>
-      <View style={GlobalStyles.genericPage}>
-        {/* <DetailsModal info="this is modal" visible={show} show={show} setShow={setShow} /> */}
+      <View style={[GlobalStyles.genericPage, styles.container]}>
         <Portal>
-          <DetailsModal show={show} hideModal={hideModal} />
+          {/* <DetailsModal info="this is modal" visible={show} show={show} setShow={setShow} /> */}
+          <AppModal
+            show={showElectionDetails}
+            hideModal={hideDetailsModal}
+            text={sampleElectionDetails}
+          />
+          <AppModal show={showTip} hideModal={hideTipModal} text={sampleTip} />
+          {/* <DetailsModal show={showElectionDetails} hideModal={hideDetailsModal} /> */}
           {/* for testing purposes */}
           {/* <BluetoothFailModal show={show} hideModal={hideModal} /> */}
         </Portal>
@@ -37,12 +56,16 @@ const HomeActivity = () => {
           <Text style={styles.headingText}>Active Elections</Text>
         </View>
         <View style={styles.electionsListContainer}>
-          <ElectionItem title="Single Choice Vote" 
-            onPress={()=> navigation.navigate("SingleChoiceVote")}
-            onLongPress={showModal} 
+          <ElectionItem
+            title="Single Choice Vote"
+            onPress={() => navigation.navigate("SingleChoiceVote")}
+            onLongPress={showDetailsModal}
           />
-          <ElectionItem title="Multiple Choice Vote" onLongPress={showModal} />
-          <ElectionItem title="Yes/No Vote" onLongPress={showModal} />
+          <ElectionItem
+            title="Multiple Choice Vote"
+            onLongPress={showTipModal}
+          />
+          <ElectionItem title="Yes/No Vote" onLongPress={showDetailsModal} />
         </View>
       </View>
     </Provider>
